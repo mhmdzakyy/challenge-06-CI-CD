1532c95426af82f44a1119c447db9e0b16f7811b
Muhammad Zaky <mhmdzakyy10@gmail.com>
2022-05-14 19:42:07 +0700

    fixing procfile, server.js

--- server.js
+++ server.js
@@ -1,3 +1,3 @@
 const app = require("./app");
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => console.log("Listening on port", PORT));
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log("Listening on port", port));
